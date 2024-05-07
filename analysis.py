f623e291c3ee9e7e7a5f89bc56430c5c9a6ae5d0
Tomasz Uszynski <tomaszuszynski3@gmail.com>
2024-05-07 21:16:36 +0100

    Added scatter() function.

--- analysis.py
+++ analysis.py
@@ -76,6 +76,41 @@ def saveHist(pd, variable_name, num_bins=10):       # Defining the saveHist func
 # Assuming 'pd' is pandas DataFrame and 'sepal_length' is the variable wanted to be plotted.
 # saveHist(pd["sepal_length"], "Sepal Length")
 
+def scatter(pd, x, y, FigureName):                      # Defining the scatter function.
+    """
+    Creates individual scatter plots for each pair of features and colors each species on the plot.
+
+    Arguments:
+        pd (pandas.DataFrame): The DataFrame containing the data.
+        x (str): Name of the x-axis feature.
+        y (str): Name of the y-axis feature.
+        FigureName (str): Name of the figure.
+    """
+    species_mapping = {                          #
+        'setosa': 'blue',                        # Mapping the species to colors.
+        'versicolor': 'green',                   # 
+        'virginica': 'orange'                    #
+    }
+
+    plt.figure(figsize = (8, 6), num=FigureName)                    # Creating a figure with a specified size.
+    for species in species_mapping:                                 # Looping through the species.
+        subset_pd = pd[pd['species'] == species]                    # Subsetting the data for each species.
+        plt.scatter(subset_pd[x], subset_pd[y], label=species)      # Creating a scatter plot for each species.
+            
+    plt.title(f"{x.capitalize().replace('_', ' ')} vs. {y.capitalize().replace('_', ' ')+'.'}",
+              fontsize = 15, fontweight = "bold")                   # Adding a title to the plot.
+    plt.xlabel(x.capitalize().replace('_', ' ') + ' (cm).',
+                fontsize = 12, fontweight = "bold")                 # Adding a label to the x-axis.
+    plt.ylabel(y.capitalize().replace('_', ' ') + ' (cm).', 
+               fontsize= 12 , fontweight = "bold")                  # Adding a label to the y-axis.
+    plt.legend()                                                    # Adding a legend to the plot.
+    plt.show()                                                      # Displaying the plot.
+    plt.close()                                                     # Closing the plot.
+
+# Example usage:
+# Assuming DataFrame is named 'pd' with columns 'sepal_length', 'sepal_width', 'petal_length', 'petal_width', and 'species':
+# scatter(pd, "sepal_length", "sepal_width", "Scatter Plot Sepal Length vs Sepal Width.")
+
 # Displaying the menu.
 def displayMenu():                                      # Defining the displayMenu function.
     print("Welcome to the Iris dataset analysis program. Please select an option from the menu below:\n")   #
@@ -97,13 +132,20 @@ while choice != "q":                                    # While the user's choic
     elif choice == "h":                                 # If the user's choice is "h", the program will execute the following code.
         print("Histograms of each variable from the Iris data set has been saved to PNG files.\n") # Printing a message to confirm that the histograms are being created.
         # Creating histograms of each variable and saving each plot to individual PNG files.
-        saveHist(pd["sepal_length"], "Sepal Length")          #
-        saveHist(pd["sepal_width"], "Sepal Width")            # Creating  the histograms for each varialbles.
-        saveHist(pd["petal_length"], "Petal Length")          #
-        saveHist(pd["petal_width"], "Petal Width")            #
+        saveHist(pd["sepal_length"], "Sepal Length")    #
+        saveHist(pd["sepal_width"], "Sepal Width")      # Creating  the histograms for each varialbles.
+        saveHist(pd["petal_length"], "Petal Length")    #
+        saveHist(pd["petal_width"], "Petal Width")      #
 
     elif choice == "p":                                 # If the user's choice is "p", the program will execute the following code.
-        print("Scatter plots for each pair of variables From the Iris data set has been created.\n") # Printing a message to confirm that the scatter plots are being created.
+        print("Scatter plots for each pair of variables from the Iris data set has been created.\n") # Printing a message to confirm that the scatter plots are being created.
+        # Creating scatter plots for each pair of variables from the Iris data set.
+        scatter(pd, "sepal_length", "sepal_width", "Scatter Plot Sepal Length vs Sepal Width.")      #  
+        scatter(pd, "sepal_length", "petal_length", "Scatter Plot Sepal Length vs Petal Length.")    #    
+        scatter(pd, "sepal_length", "petal_width", "Scatter Plot Sepal Length vs Petal Width.")      # Creating the scatter plots for each
+        scatter(pd, "sepal_width", "petal_length", "Scatter Plot Sepal Width vs Petal Length.")      #  pair of variables.
+        scatter(pd, "sepal_width", "petal_width", "Scatter Plot Sepal Width vs Petal Width.")        #
+        scatter(pd, "petal_length", "petal_width", "Scatter Plot Petal Length vs Petal Width.")      #
 
     elif choice == "a":                                 # If the user's choice is "a", the program will execute the following code.
         print ("The analysis has been saved to analysis.txt.\n") # Printing a message to confirm that the analysis has been saved.
